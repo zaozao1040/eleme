b1fabade948f75266c5d6bbd34ff5f28c593b0ac
ladygaga <zqiuning@163.com>
2018-10-08 16:08:08 +0800

    finish datashow

--- app/js/datashow/datashow_home.vue
+++ app/js/datashow/datashow_home.vue
@@ -1,6 +1,6 @@
 <template>
 	<div :class="$style.wrapper">
-		<h4>第三季度外卖大数据</h4>
+		<h4>外卖大数据&nbsp;<span>[ 第三季度 ]</span></h4>
 		<!-- <img src="./img/p1_people.png"> -->
 		<img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1538994800760&di=f5b98b685c6509e0ae6c1f772c605044&imgtype=jpg&src=http%3A%2F%2Fcdn5.rabbitpre.com%2F6ac94112-947d-4fa0-8347-cb6b2c7b52b8.png">
 	</div>
@@ -37,6 +37,9 @@ export default {
 		color: #ffeb3b;
 		font-family: serif;
 		//background: linear-gradient(#3496c7 , white 100%) 
+		span{
+			font-size: 25px;
+		}
 	}
 	img{
 		width: 100%;
